d0ae88bb43ca7b117991571c9b67772919201ee4
Marijan Mamić <mmamic@synergos.hr>
2020-10-20 18:55:28 +0200

    Added ignored Log folder.

---
+++ AdventureWorksMVC/AdventureWorks.Data/Log/DatabaseLog.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+//using NLog;
+
+namespace AdventureWorks.Data.Log
+{
+    internal static class DatabaseLog
+    {
+        //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        internal static void Write(string message)
+        {
+            Debug.Write(message);
+
+            // Log.Trace(message);
+        }
+    }
+}
